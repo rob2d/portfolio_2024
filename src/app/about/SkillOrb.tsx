c49adda5fea0fac852b2c563f885efe87345ac96
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2023-11-25 21:20:11 -0500

    update SkillOrbs, use colors, add font

--- src/app/about/SkillOrb.tsx
+++ src/app/about/SkillOrb.tsx
@@ -1,6 +1,10 @@
-import { Float, Text } from '@react-three/drei';
+import { Text, useFont } from '@react-three/drei';
 import { SkillPoint } from './SkillOrbs';
-import { useMemo } from 'react';
+import { MutableRefObject, useMemo, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { BufferAttribute, DoubleSide, Mesh, SphereGeometry } from 'three';
+
+const fontUrl = '/fonts/Futura-Bold.json';
 
 export default function SkillOrb({
   skillPoint,
@@ -9,23 +13,71 @@ export default function SkillOrb({
   skillPoint: SkillPoint;
   radianAngle: number;
 }) {
+  const font = useFont.preload(fontUrl);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const textRef = useRef() as MutableRefObject<Mesh<any>>;
+  const sphereGeoRef = useRef() as MutableRefObject<SphereGeometry>;
   const position: [number, number, number] = useMemo(
     () => [
       Math.cos(radianAngle) * 1 /* xWidth?*/,
       Math.sin(radianAngle) * 1 /* yWidth? */,
-      0
+      Math.cos(radianAngle) * 1
     ],
     [radianAngle]
   );
+
+  const sphereGeometry = useMemo(() => {
+    // Create a sphere geometry
+    const geometry = new SphereGeometry(skillPoint.value / 3, 8, 8);
+    const positionAttribute = geometry.getAttribute('position');
+
+    // Create a new color attribute
+    const colorAttribute = new BufferAttribute(
+      new Float32Array(positionAttribute.count * 3),
+      3
+    );
+
+    // Assign a random color to each vertex
+    for (let i = 0; i < colorAttribute.count; i++) {
+      colorAttribute.setXYZ(i, Math.random(), Math.random(), Math.random());
+    }
+
+    // Add the color attribute to the geometry
+    geometry.setAttribute('color', colorAttribute);
+
+    return geometry;
+  }, [skillPoint.value]);
+
+  useFrame(({ camera }) => {
+    // Make the mesh always face the camera
+    if (textRef.current) {
+      textRef.current.lookAt(camera.position);
+    }
+
+    if (sphereGeoRef.current) {
+    }
+  });
+
   return (
-    <Float scale={0.75} position={[0, 0.65, 0]} rotation={[0, 0.6, 0]}>
+    <>
       <Text
         scale={0.01 + skillPoint.value / 3}
         position={position}
-        color={'#F00'}
+        color={'#000'}
+        font={font}
+        ref={textRef}
       >
         {skillPoint.label}
       </Text>
-    </Float>
+      <mesh position={position} geometry={sphereGeometry}>
+        <meshBasicMaterial
+          opacity={0.75}
+          side={DoubleSide}
+          transparent
+          depthWrite
+          vertexColors={true}
+        />
+      </mesh>
+    </>
   );
 }
